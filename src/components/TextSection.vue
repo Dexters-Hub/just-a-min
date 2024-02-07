714e9a391d729ceda464e66b6e917af98af04bd0
Alwin John <alwinjohn231@gmail.com>
2024-02-07 12:34:36 +0530

    style:  added TextSection component

---
+++ src/components/TextSection.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  subtitle: {
+    type: String,
+    required: false
+  },
+})
+</script>
+
+<template>
+  <h1 class="text-4xl font-medium">{{ title }}</h1>
+  <p class="w-1/3 m-12 text-sm font-light text-center text-gray-500">
+    {{ subtitle }}
+  </p>
+</template>
