5324ca52d6b7ff650b4a2ee45630631492cf74ac
Alwin John <alwin.john@Alwins-MacBook-Pro.local>
2024-02-07 12:27:40 +0530

    initial commit by Vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+import TextSection from './components/TextSection.vue'
+</script>
+
+<template>
+  <main class="flex flex-col items-center justify-center min-h-screen p-4 mx-8">
+
+      <TextSection 
+        title="Select your Topic" 
+        subtitle="To play Just a Minute you must speak for a minute on a given subject. If you hesitate, repeat yourself, or deviate"
+      />
+
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <div>
+      <h1>Welcome to the Vue 3 Vite App</h1>
+    </div>
+  </header>
+  <router-view />
+</template>
